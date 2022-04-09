90c200d1590b58bdaf65e12baa777a04cd3c65bc
Priscila Roza <priscilarozasilva@gmail.com>
2022-04-09 19:56:24 -0300

    data adicionada no extrato

--- script.js
+++ script.js
@@ -2,15 +2,42 @@ let saidaSaldo = document.getElementById("saldo");
 let saidaExtrato = document.getElementById("extrato");
 let valorHTML = document.getElementById("valor");
 let linha = "<br>";
+
+function dataCurta() {
+  let data = new Date();
+  let dd = data.getDay();
+  let mm = data.getMonth();
+  let aaaa = data.getFullYear();
+  let h = data.getHours();
+  let m = data.getMinutes();
+
+  if (dd <= 9) {
+    dd = "0" + dd;
+  }
+  if (mm <= 9) {
+    mm = "0" + mm;
+  }
+  if (h <= 9) {
+    h = "0" + h;
+  }
+
+  if (m <= 9) {
+    m = "0" + m;
+  }
+  return dd + "/" + mm + "/" + aaaa + " - " + h + ":" + m;
+}
+
 let poupanca = {
   saldo: 0,
   movimentacao: [],
 
   depositar: function () {
-    // let valor = valorHTML.value;
-    this.saldo += Number(valorHTML.value);
-    saidaSaldo.innerHTML = this.saldo;
-    this.movimentacao.push("Depósito de :" + valorHTML.value);
+    let valor = Number(valorHTML.value);
+    this.saldo += valor;
+    saidaSaldo.innerHTML = "Saldo: R$ " + this.saldo.toFixed(2);
+    this.movimentacao.push(
+      dataCurta() + " - " + "Depósito R$ :" + valor.toFixed(2)
+    );
     valorHTML.value = "";
   },
   sacar: function () {
@@ -19,9 +46,11 @@ let poupanca = {
       alert("Saldo Insuficiente");
     } else {
       this.saldo -= valor;
-      this.movimentacao.push("Saque de :" + valor);
+      this.movimentacao.push(
+        dataCurta() + " - " + "Saque R$: " + valor.toFixed(2)
+      );
     }
-    saidaSaldo.innerHTML = this.saldo;
+    saidaSaldo.innerHTML = "Saldo: R$ " + this.saldo.toFixed(2);
     valorHTML.value = "";
   },
   exibirExtrato: function () {
